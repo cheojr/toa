0067a3face71b49727d84ef8c339fca0c60d6cfd
Eric Santos <eric.leinad92@gmail.com>
2014-05-08 15:30:12 -0400

    Last Version? Almost

--- bin/public_html/bin/flowdbu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NAP_BIN_PATH=/opt/nap/bin
-FILE=`date --date="10 minutes ago" "+/CassandraVol/flows/%Y/%Y-%m/%Y-%m-%d/ft-v05.%Y-%m-%d.%H%M**-0400"`
-python $NAP_BIN_PATH/flows-db-update.py $FILE 2>> /tmp/flow-errors
-python $NAP_BIN_PATH/flowsgrapher.py 2>> /tmp/flow-errors-grapher
-python $NAP_BIN_PATH/p2p_graphic.cgi
